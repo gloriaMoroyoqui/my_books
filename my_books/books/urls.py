5d5ceb994548e126dfd41f7bf955420a9a62ece0
gloriaMoroyoqui <gloriamoroyoqui8121@gmail.com>
2026-04-23 19:35:56 -0700

    hicce muuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuchos cambios

--- my_books/books/urls.py
+++ my_books/books/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('detail_book/<int:id>/', views.detail_book, name='detail_book'),
 ]
